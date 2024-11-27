684d1e04554c62a63b6b540d893c64a6f9a5de88
kishanshettyb <kishanqr@gmail.com>
2024-11-27 16:04:37 +0530

    feat: :sparkles: add navbar

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,35 +4,32 @@ import { MoveRight } from "lucide-react";
 import { TypewriterEffectSmooth } from "./typeWriterEffect";
 
 export function Banner() {
-  const words = [
-    {
-      text: "Boost",
-    },
+	const words = [
+		{
+			text: "Boost"
+		},
 
-    {
-      text: "Slaes",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-    { text: "Save" },
-    {
-      text: " Time!",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
-  return (
-    <div className="w-3/5 mt-20">
-      <h1 className="text-5xl font-bold leading-none flex flex-col justify-center items-center mb-0">
-        Launch Your Shopify Store with Ease
-        <TypewriterEffectSmooth words={words} className="mt-1" />
-      </h1>
-      <p className="text-xl opacity-50 mx-14 mb-7">
-        Make My Online Store takes care of everything from design to launch. Get
-        your store up and running in just days!
-      </p>
-      <Button variant="default">
-        Start My Store Today
-        <MoveRight />
-      </Button>
-    </div>
-  );
+		{
+			text: "Slaes",
+			className: "text-lime-500 dark:text-lime-500"
+		},
+		{ text: "Save" },
+		{
+			text: " Time!",
+			className: "text-lime-500 dark:text-lime-500"
+		}
+	];
+	return (
+		<div className="w-3/5 mt-20">
+			<h1 className="text-5xl font-bold leading-none flex flex-col justify-center items-center mb-0">
+				Launch Your Shopify Store with Ease
+				<TypewriterEffectSmooth words={words} className="mt-1" />
+			</h1>
+			<p className="text-xl opacity-50 mx-14 mb-7">Make My Online Store takes care of everything from design to launch. Get your store up and running in just days!</p>
+			<Button variant="default">
+				Start My Store Today
+				<MoveRight />
+			</Button>
+		</div>
+	);
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+	return <div className="w-full border border-x-0 border-t-0 p-4">Navbar</div>;
+}
+
+export default Navbar;
